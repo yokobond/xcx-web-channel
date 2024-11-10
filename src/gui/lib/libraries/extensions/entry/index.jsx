e414afe3100c0a9a578d81dd4eac9a77c76aa8d6
yokobond <koji.yokokawa@gmail.com>
2024-11-10 17:24:21 +0900

    chore: xcratch-create scaffolding

---
+++ src/gui/lib/libraries/extensions/entry/index.jsx
@@ -0,0 +1,49 @@
+/**
+ * This is an extension for Xcratch.
+ */
+
+import iconURL from './entry-icon.png';
+import insetIconURL from './inset-icon.svg';
+import translations from './translations.json';
+
+/**
+ * Formatter to translate the messages in this extension.
+ * This will be replaced which is used in the React component.
+ * @param {object} messageData - data for format-message
+ * @returns {string} - translated message for the current locale
+ */
+let formatMessage = messageData => messageData.defaultMessage;
+
+const entry = {
+    get name () {
+        return formatMessage({
+            id: 'xcxWebChannel.entry.name',
+            defaultMessage: 'Web Channel',
+            description: 'name of the extension'
+        });
+    },
+    extensionId: 'xcxWebChannel',
+    extensionURL: 'https://yokobond.github.io/xcx-web-channel/dist/xcxWebChannel.mjs',
+    collaborator: 'yokobond',
+    iconURL: iconURL,
+    insetIconURL: insetIconURL,
+    get description () {
+        return formatMessage({
+            defaultMessage: 'an extension for Xcratch',
+            description: 'Description for this extension',
+            id: 'xcxWebChannel.entry.description'
+        });
+    },
+    featured: true,
+    disabled: false,
+    bluetoothRequired: false,
+    internetConnectionRequired: false,
+    helpLink: 'https://yokobond.github.io/xcx-web-channel/',
+    setFormatMessage: formatter => {
+        formatMessage = formatter;
+    },
+    translationMap: translations
+};
+
+export {entry}; // loadable-extension needs this line.
+export default entry;
---
+++ src/vm/extensions/block/index.js
@@ -0,0 +1,146 @@
+import BlockType from '../../extension-support/block-type';
+import ArgumentType from '../../extension-support/argument-type';
+import Cast from '../../util/cast';
+import log from '../../util/log';
+import translations from './translations.json';
+import blockIcon from './block-icon.png';
+
+/**
+ * Formatter which is used for translation.
+ * This will be replaced which is used in the runtime.
+ * @param {object} messageData - format-message object
+ * @returns {string} - message for the locale
+ */
+let formatMessage = messageData => messageData.default;
+
+/**
+ * Setup format-message for this extension.
+ */
+const setupTranslations = () => {
+    const localeSetup = formatMessage.setup();
+    if (localeSetup && localeSetup.translations[localeSetup.locale]) {
+        Object.assign(
+            localeSetup.translations[localeSetup.locale],
+            translations[localeSetup.locale]
+        );
+    }
+};
+
+const EXTENSION_ID = 'xcxWebChannel';
+
+/**
+ * URL to get this extension as a module.
+ * When it was loaded as a module, 'extensionURL' will be replaced a URL which is retrieved from.
+ * @type {string}
+ */
+let extensionURL = 'https://yokobond.github.io/xcx-web-channel/dist/xcxWebChannel.mjs';
+
+/**
+ * Scratch 3.0 blocks for example of Xcratch.
+ */
+class ExtensionBlocks {
+    /**
+     * A translation object which is used in this class.
+     * @param {FormatObject} formatter - translation object
+     */
+    static set formatMessage (formatter) {
+        formatMessage = formatter;
+        if (formatMessage) setupTranslations();
+    }
+
+    /**
+     * @return {string} - the name of this extension.
+     */
+    static get EXTENSION_NAME () {
+        return formatMessage({
+            id: 'xcxWebChannel.name',
+            default: 'Web Channel',
+            description: 'name of the extension'
+        });
+    }
+
+    /**
+     * @return {string} - the ID of this extension.
+     */
+    static get EXTENSION_ID () {
+        return EXTENSION_ID;
+    }
+
+    /**
+     * URL to get this extension.
+     * @type {string}
+     */
+    static get extensionURL () {
+        return extensionURL;
+    }
+
+    /**
+     * Set URL to get this extension.
+     * The extensionURL will be changed to the URL of the loading server.
+     * @param {string} url - URL
+     */
+    static set extensionURL (url) {
+        extensionURL = url;
+    }
+
+    /**
+     * Construct a set of blocks for Web Channel.
+     * @param {Runtime} runtime - the Scratch 3.0 runtime.
+     */
+    constructor (runtime) {
+        /**
+         * The Scratch 3.0 runtime.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+
+        if (runtime.formatMessage) {
+            // Replace 'formatMessage' to a formatter which is used in the runtime.
+            formatMessage = runtime.formatMessage;
+        }
+    }
+
+    /**
+     * @returns {object} metadata for this extension and its blocks.
+     */
+    getInfo () {
+        setupTranslations();
+        return {
+            id: ExtensionBlocks.EXTENSION_ID,
+            name: ExtensionBlocks.EXTENSION_NAME,
+            extensionURL: ExtensionBlocks.extensionURL,
+            blockIconURI: blockIcon,
+            showStatusButton: false,
+            blocks: [
+                {
+                    opcode: 'do-it',
+                    blockType: BlockType.REPORTER,
+                    blockAllThreads: false,
+                    text: formatMessage({
+                        id: 'xcxWebChannel.doIt',
+                        default: 'do it [SCRIPT]',
+                        description: 'execute javascript for example'
+                    }),
+                    func: 'doIt',
+                    arguments: {
+                        SCRIPT: {
+                            type: ArgumentType.STRING,
+                            defaultValue: '3 + 4'
+                        }
+                    }
+                }
+            ],
+            menus: {
+            }
+        };
+    }
+
+    doIt (args) {
+        const statement = Cast.toString(args.SCRIPT);
+        const func = new Function(`return (${statement})`);
+        log.log(`doIt: ${statement}`);
+        return func.call(this);
+    }
+}
+
+export {ExtensionBlocks as default, ExtensionBlocks as blockClass};
